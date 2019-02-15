4229def42e39525e0920adc314292d3d3c31ad5a
holmeyoung <shu_liyang@163.com>
2019-02-15 18:07:30 +0800

    add train data auto make script

---
+++ tool/auto_convert_script.py
@@ -0,0 +1,98 @@
+import xml.etree.ElementTree as ET
+from os import getcwd
+import os
+import random
+import cv2
+import subprocess
+
+# -------------------------------------------------------------------------------------------------
+print ('change png and jpeg to jpg')
+files = os.listdir('JPEGImages')
+for f in files:
+    if f.split('.')[-1] != 'jpg':
+        print ('not jpg:', f)
+        img = cv2.imread('JPEGImages/'+f)
+        if img is not None:
+            cv2.imwrite('JPEGImages/'+f.split('.')[0]+'.jpg', img)
+
+        else:
+            subprocess.call('rm JPEGImages/'+f, shell=True)
+            subprocess.call('rm Annotations/'+f.split('.')[0]+'.xml', shell=True)
+            print ('img None error:', f)
+
+# -------------------------------------------------------------------------------------------------
+print ('make ImageSets/Main/*.txt')
+trainval_percent = 0.66
+train_percent = 0.5
+xmlfilepath = 'Annotations'
+txtsavepath = 'ImageSets\Main'
+total_xml = os.listdir(xmlfilepath)
+
+num=len(total_xml)
+list=range(num)
+tv=int(num*trainval_percent)
+tr=int(tv*train_percent)
+trainval= random.sample(list,tv)
+train=random.sample(trainval,tr)
+
+ftrainval = open('ImageSets/Main/trainval.txt', 'w')
+ftest = open('ImageSets/Main/test.txt', 'w')
+ftrain = open('ImageSets/Main/train.txt', 'w')
+fval = open('ImageSets/Main/val.txt', 'w')
+
+for i  in list:
+    name=total_xml[i][:-4]+'\n'
+    if i in trainval:
+        ftrainval.write(name)
+        if i in train:
+            ftrain.write(name)
+        else:
+            fval.write(name)
+    else:
+        ftest.write(name)
+
+ftrainval.close()
+ftrain.close()
+fval.close()
+ftest .close()
+
+# -------------------------------------------------------------------------------------------------
+print ('from ImageSets/Main/*.txt make train data')
+sets=['train', 'val', 'test']
+
+# classes = ["aeroplane", "bicycle", "bird", "boat"]
+classes = ["head"]
+
+def convert_annotation(image_id, list_file):
+    in_file = open('Annotations/%s.xml'%(image_id))
+    tree=ET.parse(in_file)
+    root = tree.getroot()
+
+    for obj in root.iter('object'):
+        # 有时候图像中没有要标注的物体
+        if obj.find('difficult') is None:
+            continue
+
+        difficult = obj.find('difficult').text
+        cls = obj.find('name').text
+        if cls not in classes or int(difficult)==1:
+            continue
+        cls_id = classes.index(cls)
+        xmlbox = obj.find('bndbox')
+        
+        # 有时候会出现类似 5.5 的数据，所以需要 `float` 字段转化一下
+        b = (int(float(xmlbox.find('xmin').text)), int(float(xmlbox.find('ymin').text)), int(float(xmlbox.find('xmax').text)), int(float(xmlbox.find('ymax').text)))
+        list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
+
+wd = getcwd()
+list_file = open('train.txt', 'w')
+
+for image_set in sets:
+    image_ids = open('ImageSets/Main/%s.txt'%(image_set)).read().strip().split()
+    for image_id in image_ids:
+        list_file.write('%s/JPEGImages/%s.jpg'%(wd, image_id))
+        convert_annotation(image_id, list_file)
+        list_file.write('\n')
+
+list_file.close()
+
--- tool/voc_annotation.py
+++ tool/voc_annotation.py
@@ -1,13 +1,13 @@
 import xml.etree.ElementTree as ET
 from os import getcwd
 
-sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+sets=['train', 'val', 'test']
 
-# classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+# classes = ["aeroplane", "bicycle", "bird", "boat"]
 classes = ["head"]
 
-def convert_annotation(year, image_id, list_file):
-    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
+def convert_annotation(image_id, list_file):
+    in_file = open('Annotations/%s.xml'%(image_id))
     tree=ET.parse(in_file)
     root = tree.getroot()
 
@@ -29,12 +29,12 @@ def convert_annotation(year, image_id, list_file):
 
 wd = getcwd()
 
-for year, image_set in sets:
-    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
-    list_file = open('%s_%s.txt'%(year, image_set), 'w')
+for image_set in sets:
+    image_ids = open('ImageSets/Main/%s.txt'%(image_set)).read().strip().split()
+    list_file = open('%s.txt'%(image_set), 'w')
     for image_id in image_ids:
-        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg'%(wd, year, image_id))
-        convert_annotation(year, image_id, list_file)
+        list_file.write('%s/JPEGImages/%s.jpg'%(wd, image_id))
+        convert_annotation(image_id, list_file)
         list_file.write('\n')
     list_file.close()
 
