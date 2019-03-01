2cb3325ca6a9eb282a354aa0e011df4911dff512
holmeyoung <shu_liyang@163.com>
2019-03-01 11:48:21 +0800

    change tail

--- tool/change_tail.py
+++ tool/change_tail.py
@@ -3,14 +3,17 @@
 import cv2
 import os
 
-files = os.listdir('VOCdevkit/VOC2007/JPEGImages')
+print ('change png and jpeg to jpg')
+files = os.listdir('JPEGImages')
 for f in files:
     if f.split('.')[-1] != 'jpg':
         print ('not jpg:', f)
-        img = cv2.imread('VOCdevkit/VOC2007/JPEGImages/'+f)
+        img = cv2.imread('JPEGImages/'+f)
         if img is not None:
-            cv2.imwrite('VOCdevkit/VOC2007/JPEGImages/'+f.split('.')[0]+'.jpg', img)
+            cv2.imwrite('JPEGImages/'+f.split('.')[0]+'.jpg', img)
 
         else:
+            subprocess.call('rm JPEGImages/'+f, shell=True)
+            subprocess.call('rm Annotations/'+f.split('.')[0]+'.xml', shell=True)
             print ('img None error:', f)
 
