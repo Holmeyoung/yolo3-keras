26749ecd8520310115c82f53aed5b14cbd4bfb3d
holmeyoung <shu_liyang@163.com>
2019-03-03 14:18:09 +0800

    modify_path tune

--- tool/modify_path.py
+++ tool/modify_path.py
@@ -2,14 +2,20 @@
 
 import os
 import re
+from tqdm import tqdm
+import subprocess
+
+if os.path.exists('tmp') == False:
+    os.makedirs('tmp')
+
 files = os.listdir('Annotations')
-num_total = len(files)
-num_now = 1
-for f in files:
-    print (num_now, num_total)
-    num_now += 1
+for f in tqdm(files):
     if f.split('.')[-1] != 'xml':
         print ('xml format error:', f)
+        subprocess.call('rm Annotations/%s' % (f), shell = True) 
         continue
     # re.sub(str to be changed, str to change)
-    open('tmp/'+f, 'w').write(re.sub(r'/Users/apple/Desktop/data/JPEGImages/', r'/root/tmp/data/JPEGImages/', open('Annotations/'+f).read()))
+    open('tmp/'+f, 'w').write(re.sub(r'C:\\Users\\lianjia\\Desktop\\JPEGImages\\JPEGImages\\', r'/root/tmp/yolo_1/data/JPEGImages/', open('Annotations/'+f).read()))
+
+subprocess.call('rm -r Annotations', shell = True)
+subprocess.call('mv tmp Annotations', shell = True)
